f034941bd1b9d6d0f6852c5a69fdb20e48192a55
Alex Lopez España <a20000lex@gmail.com>
2021-01-17 22:44:15 +0100

    Primer commit -nou projecte JAVA

---
+++ src/Activitat7_Intellij.java
@@ -0,0 +1,7 @@
+public class Activitat7_Intellij {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Numero +"+(i+1));
+        }
+    }
+}
