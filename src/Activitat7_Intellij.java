7f71212180d0eb1f84ac587e9ecdc2ae0a20d7c8
Alex Lopez España <a20000lex@gmail.com>
2021-01-17 22:53:34 +0100

    tercer commit hasta 35

--- src/Activitat7_Intellij.java
+++ src/Activitat7_Intellij.java
@@ -1,6 +1,6 @@
 public class Activitat7_Intellij {
     public static void main(String[] args) {
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 35; i++) {
             System.out.println("Numero +"+(i+1));
         }
     }
